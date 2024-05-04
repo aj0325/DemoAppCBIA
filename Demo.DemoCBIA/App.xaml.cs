a787755e06541e1c7bc0a72ac6ea960a1569fc4b
aj0325 <133852566+aj0325@users.noreply.github.com>
2024-05-04 14:55:47 +0530

    Add files via upload

---
+++ Demo.DemoCBIA/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Maui.ApplicationModel;
+
+namespace Demo.DemoCBIA
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Syncfusionkey");
+            //Current.RequestedTheme = OSAppTheme.Light;
+            MainPage = new AppShell();
+        }
+
+    }
+}
